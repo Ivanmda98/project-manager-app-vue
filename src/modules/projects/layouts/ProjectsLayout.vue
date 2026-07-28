e04e23e4a162c59c199d9d7c9fc8f26569bef40b
Ivan Martinez <ivan18.im67@gmail.com>
2026-07-28 16:43:37 -0600

    create top-menu aside menu components and projects route

---
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col">
+    <TopMenu></TopMenu>
+    <main>
+      <AsideMenu></AsideMenu>
+      <RouterView></RouterView>
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import AsideMenu from '../components/AsideMenu.vue';
+</script>
